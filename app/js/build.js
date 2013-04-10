89f70522e14499ebc06102d1d4a9f64ede665390
Patrik Wibron <patrik@wibron.nu>
2013-04-10 21:37:21 +0200

    Add build-file for using it with r.js optimizer

---
+++ app/js/build.js
@@ -0,0 +1,11 @@
+({
+    baseUrl: '.',
+    paths: {
+        angular: 'vendor/angular',
+        jquery: 'vendor/jquery',
+        angularui: 'vendor/angular-ui.min',
+        jqueryui: 'vendor/jquery-ui.min'
+    },
+    name: 'main',
+    out: 'main-compiled.js'
+})
