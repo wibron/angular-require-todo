b6166a7c708a73326e96cc6792df6c3d3eccd948
Patrik Wibron <patrik@wibron.nu>
2013-04-10 21:28:21 +0200

    Construct a view instead of having the view in the main file

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,5 @@
 define(['angular'], function(angular) {
-    return angular.module('todoapp', []);
+    return angular.module('todoapp', ['ui']).config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', { templateUrl: 'views/mainview.html', controller: 'TodoCtrl' });
+    }]);
 });
