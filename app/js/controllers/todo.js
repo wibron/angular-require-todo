5c1e38ba780e5cecc003a3425cf0c7a343e26acd
Patrik Wibron <patrik@wibron.nu>
2013-04-10 21:31:15 +0200

    Add logic for the todo-controller

--- app/js/controllers/todo.js
+++ app/js/controllers/todo.js
@@ -1,7 +1,50 @@
 define(['app'], function(app) {
-    return app.controller('TodoCtrl', ['$scope', 
-        function TodoCtrl ($scope) {
-            $scope.todos = [{title: 'foo'}];
+    return app.controller('TodoCtrl', ['$scope', 'filterFilter',
+        function TodoCtrl ($scope, filterFilter) {
+
+            var todos;
+
+            var getTodos = function() {
+                return JSON.parse(localStorage.getItem('todos') || '[]');
+            };
+
+            var setTodos = function(todos) {
+                localStorage.setItem('todos', JSON.stringify(todos));
+            };
+
+            $scope.todos = getTodos();
+            todos = $scope.todos;
+
+            $scope.$watch('todos', function () {
+                setTodos(todos);
+            }, true);
+
+            $scope.newTodo = function() {
+                if ($scope.todoText.length) {
+                    todos.push({ title: $scope.todoText, done: false });
+                    setTodos(todos);
+                    $scope.todoText = '';
+                }
+            };
+
+            $scope.remaining = function() {
+                var numDone = 0;
+                angular.forEach($scope.todos, function(todo) {
+                  numDone += !todo.done;
+                });
+                return numDone;
+            };
+
+            $scope.completeAll = function() {
+                angular.forEach(todos, function(todo) {
+                    todo.done = true;
+                })
+            };
+
+            $scope.verb = function() {
+                return ($scope.remaining() ===  1) ? 'item' : 'items';
+            }
+
         }
     ]);
 });
