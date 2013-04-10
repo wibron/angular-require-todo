6fbb9af04d4a5a4f17ecf3d8f9960dd3de15f551
Patrik Wibron <patrik@wibron.nu>
2013-04-10 23:50:05 +0200

    Add express-server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Todo-application built with AngularJS and requireJS
+
+The app contains a simple yet awesome express-server to serve the static files. 
+Install the dependencies by running ``` $ npm install```
+
+And then fire up the server by executing ``` $ node index.js ```
---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+    connect = require('connect'),
+    fs = require('fs');
+
+var app = express.createServer();
+app.configure(function() {
+    app.use(express.logger());
+    app.use('/app/', express.static(__dirname + '/app/'));
+    app.use(express.static(__dirname + '/app'));
+});
+
+app.listen(8080);
+console.log('Express server running on http://127.0.0.1:8080');
